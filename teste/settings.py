bb1baf6be5d9a612df1045f6e7271f39836a1353
Otto Pinto <calangodeguerra@outlook.com>
2020-06-05 08:50:57 -0300

    alowed hosts

--- teste/settings.py
+++ teste/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
+# ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0' ,'.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
