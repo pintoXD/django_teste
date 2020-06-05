bf0d9ae3b4d045bc591c85c658c91b1ae3259905
Otto Pinto <calangodeguerra@outlook.com>
2020-06-05 13:25:34 -0300

    arquivo de teste

--- myapp/teste_1.py
+++ myapp/teste_1.py
@@ -1,4 +1,7 @@
 import sys
+import os
+sys.path.insert(1,"/home/pinto/Documentos/repos/LTIs200simulator/teste_scripts")
+import teste4
 
 def print_data(input):
         for i in range(len(input)):
@@ -6,9 +9,17 @@ def print_data(input):
 
 
 def chang_stdout():
+        filename = os.path.join(os.path.dirname(__file__), "out.txt")
 
-    sys.stdout = open("out.txt", "a")
+        sys.stdout = open(filename, "a")
 
 if __name__ == "__main__":
     chang_stdout()
+    print("__file__ is", __file__)
+    te = teste4.TestSelf(5, 4)
     print_data([0,1,2,3])
+
+
+    print("Teste soma:")
+    print(te.soma(c=1))
+    print(te.a)
